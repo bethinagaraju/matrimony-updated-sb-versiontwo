5761370b42bf7d3a5cc18d7fe58415fe4eedb78c
bethinagaraju <bethinagaraju57@gmail.com>
2025-11-12 12:46:03 +0530

    aaa

--- src/main/java/com/example/demo/controller/SuperAdminController.java
+++ src/main/java/com/example/demo/controller/SuperAdminController.java
@@ -39,6 +39,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/superadmin")
 public class SuperAdminController {
